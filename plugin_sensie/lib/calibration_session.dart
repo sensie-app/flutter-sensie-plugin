6ecb633cb294da4b6b8b59bb923586a6c20fa34d
Juno <48269210+joonito@users.noreply.github.com>
2023-03-16 19:19:42 +0900

    tmp calibrationsession

--- plugin_sensie/lib/calibration_session.dart
+++ plugin_sensie/lib/calibration_session.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'types.dart';
+import 'package:sensors/sensors.dart';
 
 class CalibrationSession {
   String id;
@@ -7,15 +9,45 @@ class CalibrationSession {
   bool canCaptureSensie;
   String accessToken;
 
-  CalibrationSession(this.accessToken, this.id)
-      : currentSensie = {},
-        sensorData = SensorData(
-          [],
-          [],
-          [],
-          [],
-          [],
-          [],
-        ),
-        canCaptureSensie = true;
+  CalibrationSession({
+    required this.id,
+    required this.currentSensie,
+    required this.sensorData,
+    required this.canCaptureSensie,
+    required this.accessToken,
+  });
+
+  Map<String, StreamSubscription> startSensors(
+      {void Function(SensorData)? callback}) {
+    StreamSubscription<GyroscopeEvent>? gyroSubscription;
+    StreamSubscription<AccelerometerEvent>? accelSubscription;
+
+    SensorData sensorData = SensorData(
+      gyroX: [],
+      gyroY: [],
+      gyroZ: [],
+      accelX: [],
+      accelY: [],
+      accelZ: [],
+    );
+
+    gyroSubscription = gyroscopeEvents.listen((GyroscopeEvent event) {
+      sensorData.gyroX.add(event.x);
+      sensorData.gyroY.add(event.y);
+      sensorData.gyroZ.add(event.z);
+      if (callback != null) callback(sensorData);
+    });
+
+    accelSubscription = accelerometerEvents.listen((AccelerometerEvent event) {
+      sensorData.accelX.add(event.x);
+      sensorData.accelY.add(event.y);
+      sensorData.accelZ.add(event.z);
+      if (callback != null) callback(sensorData);
+    });
+
+    return {
+      'gyroSubscription': gyroSubscription,
+      'accelSubscription': accelSubscription
+    };
+  }
 }
